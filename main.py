451fc9e3bf9b1507b1d8e68057d6be08fcc0352a
kulvirvirk <kvirkdev@gmail.com>
2020-02-12 02:41:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+#string is just an array of chars 
+
+first_name = input('Enter first name: ')
+last_name = input('Enter last name:')
+
+#strings can be concatenated by '+' sign
+full_name = first_name + last_name
+print("Full name: " + first_name + " " + last_name)
