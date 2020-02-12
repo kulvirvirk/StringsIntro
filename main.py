870177f686f4bb73874b6a1d9ebb793aecc5a4d3
kulvirvirk <kvirkdev@gmail.com>
2020-02-12 02:48:00 +0000

    added some String functions

--- main.py
+++ main.py
@@ -6,3 +6,14 @@ last_name = input('Enter last name:')
 #strings can be concatenated by '+' sign
 full_name = first_name + last_name
 print("Full name: " + first_name + " " + last_name)
+
+
+#convert string to uppercase 
+print(full_name.upper())
+
+#print length of the string
+print(len(full_name))
+
+#check string if it contains "foo"
+x = "foo" in full_name
+print(x)
